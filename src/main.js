d2b7e42a38a8de3df7c8a856befc96d2995e281b
qiaomengyuan <qiaomengyuan@qianxin.com>
2023-06-06 20:10:53 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { lazyPlugin } from './directives'
 
 import '@/styles/common.scss'
 
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 
 app.mount('#app')

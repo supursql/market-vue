3462a5c65689b0698d407dfebab1b9b569812bc9
qiaomengyuan <qiaomengyuan@qianxin.com>
2023-06-06 19:48:01 +0800

    人气和热卖开发

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,15 @@ export function getBannerAPI () {
         url: "/home/banner"
     })
 }
+
+export function findNewAPI () {
+    return httpInstance({
+        url: "/home/new"
+    })
+}
+
+export function findHotAPI () {
+    return httpInstance({
+        url: "/home/hot"
+    })
+}
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,6 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
-import HomePannel from './components/HomePannel.vue'
 </script>
 
 <template>
@@ -15,14 +14,4 @@ import HomePannel from './components/HomePannel.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
-  <HomePannel title="新鲜好物" sub-title="新鲜好物 好多商品">
-    <div>
-      我是新鲜好物的插槽
-    </div>
-  </HomePannel>
-  <HomePannel title="人气推荐" sub-title="人气推荐 好多商品">
-    <div>
-      我是人气推荐的插槽
-    </div>
-  </HomePannel>
 </template>

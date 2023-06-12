eee4275c6fb194b007e70beaa9a3500364b07df4
qiaomengyuan <qiaomengyuan@qianxin.com>
2023-06-12 23:29:42 +0800

    401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 const httpInstance = axios.create({
     baseURL: "http://pcapi-xiaotuxian-front-devtest.itheima.net",
@@ -22,6 +23,12 @@ httpInstance.interceptors.response.use(res => res.data, e => {
         type: "warning",
         message: e.response.data.message
     })
+    if (e.response.status === 401) {
+        const userStore = useUserStore()
+        userStore.clearUserInfo()
+        router.push("/login")
+
+    }
     return Promise.reject(e)
 })
 

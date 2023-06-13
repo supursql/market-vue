9001f110b41c069ee57f36193fab3e3cbc959d0e
qiaomengyuan <qiaomengyuan@qianxin.com>
2023-06-13 16:32:24 +0800

    购物车列表统计计算

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -30,12 +30,16 @@ export const useCartStore = defineStore('cart', () => {
     const allCount = computed(() => cartList.value.reduce((a,c) => a + c.count, 0))
     const allPrice = computed(() => cartList.value.reduce((a,c) => a + c.count * c.price, 0))
     const isAll = computed(() => cartList.value.every((item) => item.selected))
+    const selectedCount = computed(() => cartList.value.filter((item) => item.selected).reduce((a,c) => a + c.count, 0))
+    const selectedPrice = computed(() => cartList.value.filter((item) => item.selected).reduce((a,c) => a + c.count * c.price, 0))
 
     return {
         cartList,
         allCount,
         allPrice,
         isAll,
+        selectedCount,
+        selectedPrice,
         singleCheck,
         addCart,
         delCart,
